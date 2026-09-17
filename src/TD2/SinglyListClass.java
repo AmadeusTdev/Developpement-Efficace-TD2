a46fb9da09e1b2bca97946172bf9b2805fd32215
Audren <audren.METERY-DROUIN@etu.univ-amu.fr>
2026-09-17 08:57:31 +0200

    ajout des classes

---
+++ src/TD2/SinglyListClass.java
@@ -0,0 +1,19 @@
+package TD2;
+
+public class SinglyListClass {
+	// Node class
+	public class Node {
+		
+	}
+	
+	// Private variables
+	private Node head;
+	private long size;
+	
+	// Constructor
+	public SinglyListClass() {
+		// Chaine vide
+		this.head = null;
+		this.size = 0;
+	}
+}
